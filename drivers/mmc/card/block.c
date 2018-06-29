c41c17561d802934b75104498540d9e0091404b5
Arne Coucheron <arco68@gmail.com>
2018-06-30 01:14:17 +0200

    mmc: Enable secure erase/trim quirk for 008GE0
    Change-Id: If03d08261680471ec814161c61de2022a881a4a2

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -3297,7 +3297,9 @@ static const struct mmc_fixup blk_fixups[] =
 	MMC_FIXUP(CID_NAME_ANY, CID_MANFID_HYNIX, CID_OEMID_ANY, add_quirk_mmc,
 		  MMC_QUIRK_BROKEN_DATA_TIMEOUT),
 
-	/* Enable secure erase/trim quirk for SEM08G and SEM16G */
+	/* Enable secure erase/trim quirk for 008GE0, SEM08G and SEM16G */
+	MMC_FIXUP("008GE0", CID_MANFID_TOSHIBA, CID_OEMID_ANY, add_quirk_mmc,
+		  MMC_QUIRK_SEC_ERASE_TRIM_BROKEN),
 	MMC_FIXUP("SEM08G", 0x45, CID_OEMID_ANY, add_quirk_mmc,
 		  MMC_QUIRK_SEC_ERASE_TRIM_BROKEN),
 	MMC_FIXUP("SEM16G", 0x45, CID_OEMID_ANY, add_quirk_mmc,
